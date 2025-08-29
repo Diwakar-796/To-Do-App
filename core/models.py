5d2cfe0b80bab66be30b36919261c7015402e4c6
Diwakar <diwakerc796@gmail.com>
2025-08-29 08:43:19 +0530

    Update models.py with latest changes

--- core/models.py
+++ core/models.py
@@ -46,7 +46,6 @@ class Donate(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     did = ShortUUIDField(unique=True, length=10, max_length=20, prefix="did", alphabet="1234567890")
 
-    # transaction_id = models.CharField(max_length=100, unique=True)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(choices=STATUS, max_length=50, default='pending')
